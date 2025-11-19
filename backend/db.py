96524c4464e5b4af09820c12af191b188dc11a24
KentJhon <dumapias.kentjhon@gmail.com>
2025-11-20 05:13:46 +0800

    config db

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,23 @@
+import os
 import mysql.connector
+from mysql.connector import Error
 
 def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",   # set if you actually use one
-        database="itrack",
-    )
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+        )
+
+        if conn.is_connected():
+            return conn
+
+        print("❌ Database connected but connection is not active.")
+        return None
+
+    except Error as e:
+        print("❌ Database connection error:", e)
+        return None
